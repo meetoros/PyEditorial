accd4fb69d73b30aa5ed1177ef0d2cf51e96fe78
meetoros <c1617025@gmail.com>
2023-12-04 17:04:14 +0300

    urls.py updated to resolve favicon.ico not found error

--- PyEditorial/urls.py
+++ PyEditorial/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include(('content.urls', 'content'), namespace='content')),
-    path('accounts/', include("django.contrib.auth.urls"))
+    path('accounts/', include("django.contrib.auth.urls")),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
 
 if settings.DEBUG:
