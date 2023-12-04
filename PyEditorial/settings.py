ec0e50d3ce69f07fca6a5986e377bc4143320b42
meetoros <c1617025@gmail.com>
2023-12-04 17:05:19 +0300

    settings.py updated for postgresql usage

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'PyEditorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'admin',
+        'PASSWORD': 'try123',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
