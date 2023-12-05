d4833157e1fc173ae324d2e5e64dc4fb025e4d52
meetoros <c1617025@gmail.com>
2023-12-06 01:49:54 +0300

    installation.sh updated

--- installation.sh
+++ installation.sh
@@ -1,6 +1,6 @@
 #bin/bash!
 
-cp ./local-default.conf ./config/nginx/conf.d/local.conf #resetting nginx server config to default
+cp ./local-default.conf ./config/nginx/conf.d/local.conf # Reset nginx server config to default. It is important to start the container
 
 
 # Add Docker's official GPG key:
@@ -20,11 +20,11 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get -y update
+sudo apt-get -y update # Update repos
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose 
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose # Install docker
 
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER # Add user to group docker
 
 sudo systemctl enable docker.service
 
@@ -32,13 +32,15 @@ sudo systemctl enable containerd.service
 
 sleep 2
 
-sudo docker compose build
+sudo docker compose build # Build 
 
-sudo docker compose up -d
+sudo docker compose up -d # Create and start in detach mode
 
-echo migration starting in 15 seconds...
+echo migration starting in 15 seconds... # Wait containers runs properly before migration
 
 sleep 15
+
+# Start migration
 
 sudo docker compose run --rm pyeditorial /bin/bash -c "./manage.py makemigrations content"
 
@@ -46,17 +48,22 @@ sudo docker compose run --rm pyeditorial /bin/bash -c "./manage.py migrate"
 
 sudo docker compose run --rm pyeditorial /bin/bash -c "./manage.py collectstatic"
 
-echo generating ssl
+# Start ssl generation
+
+echo generating ssl certificate in 5 seconds...
 
 sleep 5
 
+# Run openssl command inside nginx container with subcommand options to pass a variable TR 
 sudo docker exec -d pyeditorial-nginx-1 openssl req -x509 -nodes -subj '/C=TR' -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 
 sleep 2
 
-sudo docker cp ./local.conf pyeditorial-nginx-1:/etc/nginx/conf.d
+sudo docker cp ./local.conf pyeditorial-nginx-1:/etc/nginx/conf.d # Copy the configuration to use ssl certificate
 
 sleep 2
+
+# Restart the container to use new configuration
 
 sudo docker stop pyeditorial-nginx-1
 
